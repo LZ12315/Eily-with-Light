54f0c57bf3d7ba71cd392cfffe6510e37f09126d
Sauuuu\长安 <2514580183@qq.com>
2024-10-26 01:21:56 +0800

    更新美术资源，完善田地部分，加入城堡

---
+++ Assets/Scenes/CameraMove_Test/ParallaxBackground.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+  
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition=cameraTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 deltaMovement=cameraTransform.position-lastCameraPosition;
+        transform.position+=deltaMovement;
+        lastCameraPosition=cameraTransform.position+deltaMovement;
+    }
+}
