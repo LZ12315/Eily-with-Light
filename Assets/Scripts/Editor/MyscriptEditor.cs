c886683e0d828cfcccebffc1a036dc00796af70f
lzsworkspace-admin <yang2005222021@126.com>
2024-10-29 00:27:59 +0800

    修改错误

---
+++ Assets/Scripts/Editor/MyscriptEditor.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+using Cinemachine;
+
+[CustomEditor(typeof(CameraControlTrigger))] // ×Ô¶¨Òåinspector
+public class MyscriptEditor : Editor
+{
+    CameraControlTrigger cameraControlTrigger;
+
+    private void OnEnable()
+    {
+        cameraControlTrigger = (CameraControlTrigger)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (cameraControlTrigger.customInspectorObjects.swapCameras)
+        {
+            cameraControlTrigger.customInspectorObjects.cameraOnleft = EditorGUILayout.ObjectField("Camera on Left", cameraControlTrigger.customInspectorObjects.cameraOnleft, typeof(CinemachineVirtualCamera), true) as CinemachineVirtualCamera;
+            cameraControlTrigger.customInspectorObjects.cameraOnright = EditorGUILayout.ObjectField("Camera on Right", cameraControlTrigger.customInspectorObjects.cameraOnright, typeof(CinemachineVirtualCamera), true) as CinemachineVirtualCamera;
+        }
+        if (cameraControlTrigger.customInspectorObjects.panCameraContact)
+        {
+            cameraControlTrigger.customInspectorObjects.panDirection = (PanDirection)EditorGUILayout.EnumPopup("Camera Pan Direction", cameraControlTrigger.customInspectorObjects.panDirection);
+            cameraControlTrigger.customInspectorObjects.panDistance = EditorGUILayout.FloatField("Pan Distance", cameraControlTrigger.customInspectorObjects.panDistance);
+            cameraControlTrigger.customInspectorObjects.panTime = EditorGUILayout.FloatField("Pan time", cameraControlTrigger.customInspectorObjects.panTime);
+        }
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(cameraControlTrigger);
+        }
+    }
+}
