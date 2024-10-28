e90b75998b33fed5e9918353fb59cf72d1486c60
lzsworkspace-admin <yang2005222021@126.com>
2024-10-28 17:22:05 +0800

    基本完成震荡波效果

---
+++ Assets/Scripts/Effects/ShockWaveControl.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockWaveControl : MonoBehaviour
+{
+    public float shockWaveTime = 0.5f;
+
+    private Coroutine shockWaveCorotine;
+
+    [SerializeField]private Material material;
+
+    [SerializeField]private static int WaveDistanceFromCenter = Shader.PropertyToID("_WaveDistanceFromCenter");
+
+    private void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CallShockWave();
+        }
+    }
+
+    public void CallShockWave()
+    {
+        shockWaveCorotine = StartCoroutine(ShockWaveAction(-0.1f, 1f));
+    }
+
+    private IEnumerator ShockWaveAction(float startPos, float endPos)
+    {
+        material.SetFloat(WaveDistanceFromCenter, startPos);
+
+        float lerpedAmount = 0f;
+        float elapsedTime = 0f;
+        while(elapsedTime <= shockWaveTime)
+        {
+            elapsedTime += Time.deltaTime;
+            lerpedAmount = Mathf.Lerp(startPos, endPos, elapsedTime / shockWaveTime);
+
+            material.SetFloat(WaveDistanceFromCenter, lerpedAmount);
+            yield return null;
+        }
+
+        material.SetFloat(WaveDistanceFromCenter, -0.1f);
+    }
+}
