c9a8fc98dacfadc3d48ad36b6773b42284030707
Heartbea7 <1729111805@qq.com>
2024-10-28 12:26:52 +0800

    第一关光球摆了一下

--- Assets/Scripts/Manager/TimelineManager.cs
+++ Assets/Scripts/Manager/TimelineManager.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,13 +10,22 @@ public class TimelineManager : MonoBehaviour
     public GameObject[] triggerObjs;
     public PlayableDirector[] timelineDirectors;
     public bool isPlaying;
+    public int curDirector=0;
+    public CinemachineVirtualCamera follow1;
+    public CinemachineVirtualCamera follow2;
+    private CharCtrl CharCtrl;
 
+    void Awake()
+    {
+        transform.position = new Vector3(75.53f, -12.2f, 0);
+    }
     void OnTriggerEnter2D(Collider2D trigger)
     {
         for (int i = 0; i < triggerObjs.Length; i++)
         {
             if (trigger.gameObject == triggerObjs[i])
             {
+                curDirector = i + 1;
                 PlayTimeline(timelineDirectors[i + 1]);
             }
         }
@@ -35,4 +45,12 @@ public class TimelineManager : MonoBehaviour
         isPlaying= false;
     }
 
+    public void SwitchToSmall()
+    {
+        transform.localScale *=0.3f;
+        CharCtrl.particle.transform.localScale *= 0.3f;
+        follow1.gameObject.SetActive(false);
+        follow2.gameObject.SetActive(true);
+
+    }
 }
